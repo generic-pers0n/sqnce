b7a8c9ac5dbd62983ac37f3630eae8332f54dd51
Avery King <avery98@pm.me>
2019-12-10 19:07:06 -0800

    Add support for sequences with a non-constant common difference
    Before, libsqnce could only find an arithmetic sequence that had a common
    difference that was constant. Now, libsqnce should hopefully have improved
    support for finding arithmetic sequences that do not have constant common
    differences.

--- libsqnce.py
+++ libsqnce.py
@@ -25,19 +25,43 @@ def clear():
     os.system("cls")
 
 def common_difference(term_1, term_2, term_3, term_4):
-  common_diff = 0 # Common difference
+  common_diff_1 = 0 # Common difference
+  common_diff_2 = 0
+  common_diff_overall = 0
 
   term_1_inc = term_1
+  term_2_inc = term_2
 
   # We know that term 1 and term 2 will not have the same value,
-  # so we need to increment the diff
-  while (term_1_inc != term_2):
-    common_diff += 1 # Increment common_diff
+  # so we need to increment the common difference. When we
+  # increment the common difference, we basically count it.
+  while (term_1 != term_2):
+    common_diff_1 += 1 # Increment common_diff
     term_1_inc += 1
 
     # If term 1 is equal to term 2, then break
-    if (term_1 == term_2):
+    if (term_1_inc == term_2):
+      common_diff_overall = common_diff_1
       break;
 
-  print("The common difference is ", common_diff)
+    # If the common difference is NOT constant, subtract the 2nd
+    # common difference variable from the first common differece
+    # variable. We'll assume that this will be constant. In other
+    # words, the common difference increases per term (e.g. 1, 2, 5, 10...,
+    # the common difference is increasing by 2 (1 to 3 to 5 and etc).
+    if (term_1_inc != term_2):
+      while (ter_1_inc != term_2):
+        common_diff_2 +=1 # Increment this until it equals term 3
+        term_2_inc += 1
+
+        if (term_2_inc == term_3):
+          common_diff_overall = common_diff_2
+
+  # That should work for arithmetic sequences that either have a
+  # constant or a non-constant common difference. Either me
+  # (@generic_pers0n) or Mario (@Megitsune-MA) will figure out how
+  # to solve geometric sequences one time. For now, that remains a
+  # mystery.
+
+  print("The common difference is ", common_diff_overall)
 
